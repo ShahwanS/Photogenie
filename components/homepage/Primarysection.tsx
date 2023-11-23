eda3aaf6a38b3bcabe9dafbc3649b55ca0edc8d0
Saif Shahwan <Saif_shahwan@hotmail.com>
2023-11-23 02:14:12 +0100

    Refactor homepage Primarysection component

--- components/homepage/Primarysection.tsx
+++ components/homepage/Primarysection.tsx
@@ -3,17 +3,15 @@ import React from "react";
 
 const Primarysection = () => {
   return (
-    <div className="flex flex-col h-auto items-start  bg-[#1E1E1E] text-white p-4 sm:p-8 space-y-8 w-full">
-      <div className="md:space-y-20 space-y-10 max-w-6xl ">
-        <h1 className="text-3xl sm:text-6xl font-bold mb-2">
-          Capturing the Essence of Marburg Through Photography
-        </h1>
-        <p className="text-lg sm:text-2xl">
-          Experience the beauty and charm of Marburg city through the lens of
-          our talented photographer.
-        </p>
+    <div className="justify-between self-stretch bg-stone-900 flex flex-col px-16 max-md:px-5">
+      <div className="self-stretch text-white text-6xl font-bold leading-[67px] mt-20 max-md:max-w-full max-md:text-4xl max-md:leading-[54px] max-md:mt-10">
+        Capturing the Essence of Marburg Through Photography
       </div>
-      <div className="sm:flex sm:flex-row space-x-4 sm:space-y-0 sm:space-x-4">
+      <div className="self-stretch text-white text-lg leading-7 mt-10 max-md:max-w-full">
+        Experience the beauty and charm of Marburg city through the lens of our
+        talented photographer.
+      </div>
+      <div className="items-stretch flex w-[227px] max-w-full gap-4 mt-10 mb-20 max-md:mb-10">
         <Link
           href="/Contact"
           className="px-4 sm:px-6 py-2 sm:py-3 bg-[#2D2D2D] rounded-xl hover:bg-gray-700 transition duration-300 ease-in-out text-center"
